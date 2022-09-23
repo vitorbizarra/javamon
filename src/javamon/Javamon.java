5f419a398e7e01c53708a9690fa76e86858b5740
vitorbizarra <vitorbizarracontato@gmail.com>
2022-09-22 23:13:18 -0300

    first commit

---
+++ src/javamon/Javamon.java
@@ -0,0 +1,9 @@
+package javamon;
+
+public class Javamon {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/javamon/models/Pokemon.java
@@ -0,0 +1,79 @@
+package javamon.models;
+
+public class Pokemon {
+    private String nome;
+    private String tipo;
+    private int level;
+    private int vida;
+    private int ataque;
+    private int defesa;
+    private String sprite_front;
+    private String sprite_back;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getDefesa() {
+        return defesa;
+    }
+
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+
+    public String getSprite_front() {
+        return sprite_front;
+    }
+
+    public void setSprite_front(String sprite_front) {
+        this.sprite_front = sprite_front;
+    }
+
+    public String getSprite_back() {
+        return sprite_back;
+    }
+
+    public void setSprite_back(String sprite_back) {
+        this.sprite_back = sprite_back;
+    }
+    
+    
+   
+}
